b1593d36a2e0f9c8e2d12ab088181a1626c00f32
jdrabot <jdrabot@hotmail.com>
2021-04-25 00:06:44 -0400

    finished category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -40,6 +40,7 @@ router.get('/:id', async (req, res) => {
       return;
     }
     res.status(200).json(oneCategory);
+    
   } catch (err) {
     res.json(500).json(err);
   }
@@ -48,14 +49,54 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const createCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    if (!createCategory) {
+      res.status(404).json({ message: "New category not created!" });
+      return;
+    }
+    res.status(200).json(createCategory);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!updateCategory) {
+      res.status(404).json({ message: "Category not updated!" });
+      return;
+    }
+    res.status(200).json(updateCategory)
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deleteCategory) {
+      res.status(404).json({ message: "no category to be deleted!" });
+      return;
+    }
+    res.status(200).json(deleteCategory)
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
