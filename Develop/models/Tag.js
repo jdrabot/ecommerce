679181d45b587642057ba8965614b9a003188a4f
jdrabot <jdrabot@hotmail.com>
2021-04-24 21:43:09 -0400

    add category names and comment fo the tag table

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -3,7 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
-
+// adds names for the tag table
 Tag.init(
   {
     id: {
