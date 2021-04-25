b7f96c8f9c9ae6127187773585e4796dc62e3459
jdrabot <jdrabot@hotmail.com>
2021-04-24 23:44:50 -0400

    added route to get all categories in the category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,8 +3,21 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: [ "id", "price", "product_name", "stock", "category_id"],
+        },
+      ],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
